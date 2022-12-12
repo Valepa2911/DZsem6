7c2cdf658f31077737a0a9d42b436e3579081b7a
Valepa2911 <megp17102017@mail.ru>
2022-12-12 19:38:41 +0400

    сохран.

---
+++ Zadacha43/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//Точки для уравнений пользователь вводит с клавиатуры, можно через несколько ReadLine
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите 4 числа через запятую(k1,k2,b1,b2): ");
+double[] mass = Array.ConvertAll(Console.ReadLine(). Split(",") , double. Parse);
+double k1 = mass[0];
+double k2 = mass[1];
+double b1 = mass[2];
+double b2 = mass[3];
+int Len = mass.Length;
+//double x = -(b1 - b2) / (k1 - k2);
+//double y = k1 * x + b1;
+Uravnenie (mass);
+
+
+
+void Uravnenie (double[] mass)
+{
+    double x = -(b1 - b2) / (k1 - k2);
+    double y = k1 * x + b1;
+        
+    Console.WriteLine($"Пересечение в точке: {x};{y}");
+}
